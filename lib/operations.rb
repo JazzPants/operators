bba3be6553dd66d663574dbe248a3c870dc1e53a
JazzPants <leighton.l14@gmail.com>
2022-01-18 17:23:32 +1100

    commit tree 926a51b19d2f95d090bb2a6dc0700f7565d33e55

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,17 @@
-def unsafe?(speed)
+require "pry"
+#1 gem install pry
+#2 learn test
 
+def unsafe?(speed)
+    binding.pry
+    if speed < 40 && speed > 60
+        return true
+    else
+        return false
+    end
 end
 
-
+unsafe?(30)
 
 def not_safe?(speed)
 	
