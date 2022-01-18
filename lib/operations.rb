ced8486216e7f51bab266dd3b7caccc2712c8c01
JazzPants <leighton.l14@gmail.com>
2022-01-18 17:31:34 +1100

    Done!

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,21 @@
-def unsafe?(speed)
+require "pry"
+#in code block put binding.pry
+#1 gem install pry
+#2 learn test
 
+def unsafe?(speed)
+    
+    if speed < 40 || speed > 60
+        return true
+    else
+        return false
+    end
 end
 
-
+unsafe?(30)
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
